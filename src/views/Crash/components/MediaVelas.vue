698fcec475fa8c95dda73c3852aa67d35498a167
Anderson Luiz <aluiz@thoughtworks.com>
2023-06-16 17:44:55 -0300

    add media vela 100x

--- src/views/Crash/components/MediaVelas.vue
+++ src/views/Crash/components/MediaVelas.vue
@@ -9,28 +9,38 @@ console.log(props.media_intervalos)
       <h5 class="card-title">Média de velas</h5>
     </div>
     <div class="card-body">
-      <div style="display: flex;">
+      <div >
+        <div style="display: flex;">
+          <div class="card" style="width: auto; height: fit-content;">
+            <div class="card-body">
+              <h5 class="card-title">3X</h5>
+              <p class="card-text"><mark>{{ media_intervalos['3x'] }}</mark></p>
+            </div>
+          </div>
 
-        <div class="card" style="width: auto;">
-          <div class="card-body">
-            <h5 class="card-title">3X</h5>
-            <p class="card-text"><mark>{{ media_intervalos['3x'] }}</mark></p>
+          <div class="card" style="width: auto; height: fit-content; margin: 0 10px;">
+            <div class="card-body">
+              <h5 class="card-title">5X</h5>
+              <p class="card-text"><mark>{{ media_intervalos['5x'] }}</mark></p>
+            </div>
+          </div>
+
+          <div class="card" style="width: auto; height: fit-content">
+            <div class="card-body">
+              <h5 class="card-title">10X</h5>
+              <p class="card-text"><mark>{{ media_intervalos['10x'] }}</mark></p>
+            </div>
+          </div>
+        </div>
+        <div style="display: flex;margin-top: 10px;">
+          <div class="card" style="width: auto; height: fit-content">
+            <div class="card-body">
+              <h5 class="card-title">100X</h5>
+              <p class="card-text"><mark>{{ media_intervalos['100x'] }}</mark></p>
+            </div>
           </div>
         </div>
 
-        <div class="card" style="width: auto; margin: 0 10px;">
-          <div class="card-body">
-            <h5 class="card-title">5X</h5>
-            <p class="card-text"><mark>{{ media_intervalos['5x'] }}</mark></p>
-          </div>
-        </div>
-
-        <div class="card" style="width: auto;">
-          <div class="card-body">
-            <h5 class="card-title">10X</h5>
-            <p class="card-text"><mark>{{ media_intervalos['10x'] }}</mark></p>
-          </div>
-        </div>
       </div>
     </div>
   </div>
