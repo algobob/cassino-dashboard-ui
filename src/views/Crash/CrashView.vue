6073908260335b5a648c8c6ce953982d84dc8906
Anderson Luiz <algobob@gmail.com>
2023-06-18 13:11:15 -0300

    add estrategias + dashboard api call

--- src/views/Crash/CrashView.vue
+++ src/views/Crash/CrashView.vue
@@ -6,28 +6,27 @@ import BaseLayout from "../../layouts/sections/components/BaseLayout.vue";
 import View from "../../layouts/sections/components/View.vue";
 
 import MediaVelas from "./components/MediaVelas.vue"
+import PadraoEstrategias from "./components/PadraoEstrategias.vue"
 import ContagemCores from "./components/ContagemCores.vue"
 import GraficoLinha from "./components/GraficoLinha.vue"
-import MaterialInput from "@/components/MaterialInput.vue";
-import MaterialButton from "@/components/MaterialButton.vue";
 import MaterialAlert from "@/components/MaterialAlert.vue";
 
 //nav-pills
 import setNavPills from "@/assets/js/nav-pills";
 
-const media_intervalos = ref({})
+const estrategias = ref({})
 const contagem_cores = ref({})
+const media_intervalos = ref({})
 const qtd_velas = ref("200")
-const loadClicked = ref(false)
 
 const load = () => {
-  fetch('/creu/blaze/crash/media/velas?max_velas='+qtd_velas.value)
+  fetch('/creu/blaze/dashboard/'+qtd_velas.value)
   .then(response => response.json())
-  .then(data => media_intervalos.value = data)
-
-  fetch('/creu/blaze/crash/contagemCores?max_velas='+qtd_velas.value)
-  .then(response => response.json())
-  .then(data => contagem_cores.value = data)
+  .then(data => {
+    estrategias.value = data['estrategias']
+    contagem_cores.value = data['contagem_cores']
+    media_intervalos.value = data['media_intervalos']
+  })
 }
 
 //hook
@@ -50,15 +49,16 @@ name="qtdVelas"
     /> 
 </label>
  <button class="mt-2" @click="load">Load</button>
- <MaterialAlert v-if="loadClicked" class="mt-2" style="width: fit-content;" color="success" fontWeight="bold">
+ <!-- <MaterialAlert v-if="loadClicked" class="mt-2" style="width: fit-content;" color="success" fontWeight="bold">
 Atualizado com sucesso!
-</MaterialAlert>
+</MaterialAlert> -->
 </div>
 
     <div style="display: flex;" class="mt-2">
+      <PadraoEstrategias :estrategias="estrategias" />
       <MediaVelas :media_intervalos="media_intervalos" />
       <ContagemCores :contagem_cores = "contagem_cores" />
-      <GraficoLinha :contagem_cores = "contagem_cores" />
+      <!-- <GraficoLinha :contagem_cores = "contagem_cores" /> -->
     </div>
    
   </BaseLayout>
---
+++ src/views/Crash/components/PadraoEstrategias.vue
@@ -0,0 +1,36 @@
+<style scoped>
+.numberCircle {
+  border-radius: 50%;
+  width: 55px;
+  height: 55px;
+  padding: 9px;
+
+  background: greenyellow;
+  border: 2px solid #666;
+  color: #666;
+  text-align: center;
+
+  font: 18px Arial, sans-serif;
+}
+</style>
+
+<script setup>
+const props = defineProps(['estrategias'])
+</script>
+<template>
+  <div class="card" style="width: 35%;">
+    <div class="card-header">
+      <h5 class="card-title">Estrategias - Probabilidades</h5>
+    </div>
+    <div class="card-body">
+      <div style="display: flex; align-items: center;" class="mb-2">
+        <div class="numberCircle">5X</div>
+        <h4 class="m-2">+ 5 min ====> {{ props.estrategias['5x5min'] }}</h4>
+      </div>
+      <div style="display: flex; align-items: center;" class="mb-2">
+        <div class="numberCircle">10X</div>
+        <h4 class="m-2">+ 5 min ====> {{ props.estrategias['10x5min'] }}</h4>
+      </div>
+    </div>
+  </div>
+</template>
