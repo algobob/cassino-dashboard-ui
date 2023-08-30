429f04cb73395256043dc9112bf73df7b36a8749
Anderson Luiz <algobob@gmail.com>
2023-08-30 09:28:53 -0300

    add grafico linha component

--- src/views/Crash/components/GraficoLinha.vue
+++ src/views/Crash/components/GraficoLinha.vue
@@ -1,47 +1,83 @@
 <template>
-  <Line
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
+  <div class="example">
+    <VueApexCharts
+      width="800"
+      height="500"
+      type="line"
+      :options="chartOptions"
+      :series="series"
+    ></VueApexCharts>   
+  </div>
 </template>
 
-<script>
-import { Line } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-} from 'chart.js'
+<script setup>
+import { ref } from "vue";
+import VueApexCharts from "vue3-apexcharts";
+import * as _ from 'lodash'
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-)
+const props = defineProps(['velas'])
 
-export default {
-  name: 'LineChart',
-  components: { Line },
-  data() {
-    return {
-      chartData: {
-        labels: [ '1x', '2x', '3x', '4x', '5x', '6x', '7x', '8x', '9x', '10x+'],
-        datasets: [ { data: [1.6, 652.62, 1.39, 2.59, 1.43, 1.61, 4.15, 1.28, 3.16, 1.2, 9.12] } ]
-      },
-      chartOptions: {
-        responsive: true
-      }
-    }
-  }
+
+const groupBy = (velas) => {
+  const result = {}
+  velas.forEach( v => {
+    const d = new Date(v.created) 
+    const minutesBase = Math.floor(d.getMinutes() / 5) * 5;
+    d.setMinutes(minutesBase)
+    d.setSeconds(0)
+
+    const key = d.toISOString()
+
+    if (key in result) {      
+      result[key].push(v.vela)
+    } else {      
+      result[key] = [v.vela]
+    }    
+  })
+
+  console.log('velasGroupedBy5min ', result)
+
+  for (const [key, value] of Object.entries(result)) {
+      result[key] = _.mean(value)
+    } 
+  
+  return result
 }
+
+const velasGroupedBy5min = groupBy(props?.velas)
+
+// const dataSeries = props?.velas?.map( v => [v.created, v.vela ])
+const dataSeries = []
+for (const [key, value] of Object.entries(velasGroupedBy5min)) {
+  dataSeries.push([key, value.toFixed(2)])
+}  
+console.log('dataSeries ', dataSeries)
+
+
+let chartOptions = ref({
+  chart: {
+    type: "line",
+    height: 350,
+  },
+  title: {
+    text: "Grafico de linha - Velas",
+    align: "left",
+  }, 
+  xaxis: {
+    type: "datetime",
+  },
+  yaxis: {
+    tooltip: {
+      enabled: true,
+    },
+    max: 20
+  },
+});
+
+const series = ref([
+  {
+    data: dataSeries
+  },
+]);
+
 </script>
