3099a7ac308d3384fc85057ac5a1bccc244bd4c0
Anderson Luiz <algobob@gmail.com>
2023-10-22 12:49:58 -0300

    fix + white target color

--- src/views/Cassino/components/double/padroes/surf/PadraoCor.vue
+++ src/views/Cassino/components/double/padroes/surf/PadraoCor.vue
@@ -54,6 +54,7 @@ const props = defineProps({
 
 </script>
 <template>
+  <div v-if="data">
   <div style="display: flex; align-items: center; position: relative;" v-if="data['red']">
     <span class="tooltiptext" v-if="hover">{{ data['red']['hit'] }}/{{ data['red']['tried'] }}</span>
     <div v-for="p in padrao" @mouseover="hover = true" @mouseleave="hover = false"
@@ -72,4 +73,14 @@ const props = defineProps({
     <h5 class="font-weight-bold p-2"> ? </h5>
     <h5 class="font-weight-bold p-2"><mark>{{ data['black']['probabilidade'] }}%</mark></h5>
   </div>
+  <div style="display: flex; align-items: center; position: relative;" v-if="data['white']">
+    <span class="tooltiptext" v-if="hover">{{ data['white']['hit'] }}/{{ data['white']['tried'] }}</span>
+    <div v-for="p in padrao" @mouseover="hover = true" @mouseleave="hover = false"
+    :class="{ 'red-circle': p === 'r', 'black-circle': p === 'b', 'white-circle': p === 'w' }"></div>
+    <h5 class="font-weight-bold p-2"> > </h5>
+    <div class="white-circle"></div>
+    <h5 class="font-weight-bold p-2"> ? </h5>
+    <h5 class="font-weight-bold p-2"><mark>{{ data['white']['probabilidade'] }}%</mark></h5>
+  </div>
+</div>
 </template>
