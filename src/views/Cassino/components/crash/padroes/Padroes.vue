359a0279da414c7c9f6e0fd52098730d7280936b
Anderson Luiz <algobob@gmail.com>
2023-11-01 10:49:40 -0300

    make Padroes section expandable

--- src/views/Cassino/components/crash/padroes/Padroes.vue
+++ src/views/Cassino/components/crash/padroes/Padroes.vue
@@ -1,4 +1,8 @@
 <script setup>
+import { Collapse } from 'vue-collapsed'
+import { ref } from 'vue'
+
+const isExpanded = ref(true)
 const props = defineProps({
   padroes: Object
 })
@@ -9,31 +13,27 @@ import Padrao from "./Padrao.vue"
 </script>
 <template>
   <div style="display: flex; flex-direction: column; flex-wrap: wrap; margin-top: 60px;" v-if="padroes">
-    <h5 style="text-decoration: underline;">Padroes</h5>
-
-        <div style="display: flex;  gap: 60px;">
+    <h4 style="text-decoration: underline; margin-bottom: 30px;" @click="isExpanded = !isExpanded">Padroes</h4>
+    <Collapse :when="isExpanded">
+      <div style="display: flex;  gap: 60px;">
         <div>
-          <Padrao :padrao="[2,2,2]" :data="padroes['2,2,2']" />
-          <Padrao :padrao="[2,1,2]" :data="padroes['2,1,2']" />
-          <Padrao :padrao="[1,2,1]" :data="padroes['1,2,1']" />
-          <Padrao :padrao="[2,2,1]" :data="padroes['2,2,1']" />
-          <Padrao :padrao="[2,2,1,1]" :data="padroes['2,2,1,1']" />
-          <Padrao :padrao="[1,1,2,2]" :data="padroes['1,1,2,2']" />
-          <Padrao :padrao="[2,2,2,2]" :data="padroes['2,2,2,2']" />
-          <Padrao :padrao="[2,2,2,2,2]" :data="padroes['2,2,2,2,2']" />
-          <Padrao :padrao="[1,1,1]" :data="padroes['1,1,1']" />
-          <Padrao :padrao="[1,1,1,1]" :data="padroes['1,1,1,1']" />
-          <Padrao :padrao="[1,1,1,1,1]" :data="padroes['1,1,1,1,1']" />
-          <Padrao :padrao="[1,1,1,1,1,1]" :data="padroes['1,1,1,1,1,1']" />
-          <Padrao :padrao="[1,1,1,1]" :data="padroes['1,1,1,1']" />
-          <Padrao :padrao="[2,1,2,1]" :data="padroes['2,1,2,1']" />
-          <Padrao :padrao="[2,1,2,1,2,1]" :data="padroes['2,1,2,1,2,1']" />
-          <!-- <PadraoSurf :qtd="3" :isGreen="true" :data="surf?.verde[3]" /> -->
-        </div>
-        <div>
-          <!-- <PadraoSurf :qtd="2" :isGreen="false" :data="surf?.preto[2]" />
-          <PadraoSurf :qtd="3" :isGreen="false" :data="surf?.preto[3]" /> -->
+          <Padrao :padrao="[2, 2, 2]" :data="padroes['2,2,2']" />
+          <Padrao :padrao="[2, 1, 2]" :data="padroes['2,1,2']" />
+          <Padrao :padrao="[1, 2, 1]" :data="padroes['1,2,1']" />
+          <Padrao :padrao="[2, 2, 1]" :data="padroes['2,2,1']" />
+          <Padrao :padrao="[2, 2, 1, 1]" :data="padroes['2,2,1,1']" />
+          <Padrao :padrao="[1, 1, 2, 2]" :data="padroes['1,1,2,2']" />
+          <Padrao :padrao="[2, 2, 2, 2]" :data="padroes['2,2,2,2']" />
+          <Padrao :padrao="[2, 2, 2, 2, 2]" :data="padroes['2,2,2,2,2']" />
+          <Padrao :padrao="[1, 1, 1]" :data="padroes['1,1,1']" />
+          <Padrao :padrao="[1, 1, 1, 1]" :data="padroes['1,1,1,1']" />
+          <Padrao :padrao="[1, 1, 1, 1, 1]" :data="padroes['1,1,1,1,1']" />
+          <Padrao :padrao="[1, 1, 1, 1, 1, 1]" :data="padroes['1,1,1,1,1,1']" />
+          <Padrao :padrao="[1, 1, 1, 1]" :data="padroes['1,1,1,1']" />
+          <Padrao :padrao="[2, 1, 2, 1]" :data="padroes['2,1,2,1']" />
+          <Padrao :padrao="[2, 1, 2, 1, 2, 1]" :data="padroes['2,1,2,1,2,1']" />
         </div>
       </div>
+    </Collapse>
   </div>
 </template>
